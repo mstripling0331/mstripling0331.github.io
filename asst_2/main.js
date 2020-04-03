784c13f16d55e6f7d0a9ddfbc283f96d08039ec3
mstripling0331 <60369788+mstripling0331@users.noreply.github.com>
2020-04-02 21:44:30 -0400

    Implemented darken/lighten button

--- asst_2/main.js
+++ asst_2/main.js
@@ -5,20 +5,28 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
-/*
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);*/
-
 for (let i = 1; i < 5; i++){
     const newImage = document.createElement('img');
     newImage.setAttribute('src', "images/pic" + i +".jpg");
     thumbBar.appendChild(newImage);
+
+    /* onclick handler */
     newImage.onclick = function(p){
         displayedImage.setAttribute('src', p.target.getAttribute('src'));
     }
 
 }  
 
-
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function(){
+        if(btn.getAttribute('class') === "dark"){
+            btn.setAttribute('class', "light");
+            btn.innerHTML = "Lighten";
+            overlay.style.backgroundColor =  "rgba(0,0,0,0.5)";
+        }
+        else{
+            btn.setAttribute('class', "dark");
+            btn.innerHTML = "Darken";
+            overlay.style.backgroundColor =  "rgba(0,0,0,0)";
+        }
+    }
