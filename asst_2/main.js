a4b37d2415cc3cb99c352b92a3cbfe63ba5a96d6
mstripling0331 <60369788+mstripling0331@users.noreply.github.com>
2020-04-02 21:19:13 -0400

    Added image loop

--- asst_2/main.js
+++ asst_2/main.js
@@ -6,8 +6,19 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
+/*
 newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+thumbBar.appendChild(newImage);*/
+
+for (let i = 1; i < 5; i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', "images/pic" + i +".jpg");
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(p){
+        displayedImage.setAttribute('src', p.target.getAttribute('src'));
+    }
+
+}  
+
 
 /* Wiring up the Darken/Lighten button */
